0cf15b04845ce31f2efcc1588547ec896914c8f9
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2022-01-19 22:14:16 +0530

    fixes

--- src/Components/Nftdetails/Nftdetails.js
+++ src/Components/Nftdetails/Nftdetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import bandanapunk from '../../assets/5.jpg'
+import acc from '../../assets/acc.png'
 
 function Nftdetails() {
     return (
@@ -9,7 +10,19 @@ function Nftdetails() {
             </div>
             <div>
                 <p className='text-8xl text-white py-8 font-extrabold'>Bandana Punk<span className="text-gray-400 text-7xl">#0</span></p>
+                <div className='flex gap-2 pt-[10vh]'>
+                    <img className=" rounded-full w-12" src={acc} alt="acc" />
 
+                    <div className='flex flex-col justify-center items-start'>
+                        <p className='text-white'>0x91eedc8077da7ee81b76ffe482257040f5cfa025</p>
+                        <p className='text-btn'>sanskargupta0901@gmail.com</p>
+                    </div>
+                    <div>
+                        <div className='rounded-full w-24'>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
